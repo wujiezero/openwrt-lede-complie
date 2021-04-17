7a801a738801c9700adab5f608c05dea393149ed
wujiezero <36733410+wujiezero@users.noreply.github.com>
2021-04-17 12:19:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,7 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/MentoHUST-OpenWrt-ipk
-git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+
+#git lua-maxminddb
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git
